e4b778f070ab019c3d08354f52d9015ac334dec9
Navya Srivastava <143245639+NSrivastava1012@users.noreply.github.com>
2025-05-10 13:28:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-// Optional: Disable button on submit to show feedback
-document.getElementById('spamForm').addEventListener('submit', function() {
-    const button = document.querySelector('button');
-    button.innerText = "Checking...";
-    button.disabled = true;
-});
